5851f3cd308915492b767a8bf453a575d6895f1f
節點活動合約 <>
2023-12-28 12:07:16 +0800

    改名

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
 
-        applicationId = "li.songe.gkd"
+        applicationId = "com.quickly.kyd"
         versionCode = 14
         versionName = "1.5.4"
 
